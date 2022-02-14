73acba9e03abd923276453aaa98cd785ab09c5e9
marijmokoginta <marijmokoginta04@gmail.com>
2022-02-14 11:00:51 +0800

    update

--- src/main/java/com/ecommerce/EcommerceApiApplication.java
+++ src/main/java/com/ecommerce/EcommerceApiApplication.java
@@ -1,18 +1,10 @@
 package com.ecommerce;
 
-import com.ecommerce.models.entities.Role;
-import com.ecommerce.models.entities.UserApp;
-import com.ecommerce.services.UserService;
 import org.modelmapper.ModelMapper;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.util.Date;
-
-import static com.ecommerce.constant.SecurityConstant.ROLE_ADMIN;
-import static com.ecommerce.constant.SecurityConstant.ROLE_USER;
 
 @SpringBootApplication
 public class EcommerceApiApplication {
@@ -25,19 +17,4 @@ public class EcommerceApiApplication {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
-
-    @Bean
-    public CommandLineRunner run(UserService userService) {
-        return args -> {
-            Role user = userService.save(new Role(null, ROLE_USER));
-            Role admin = userService.save(new Role(null, ROLE_ADMIN));
-
-            UserApp userApp = userService.save(new UserApp(1, "Administrator", null, "marijmokoginta21@gmail.com",
-                    "123asad", null, null, null, new Date(System.currentTimeMillis()),
-                    true, true));
-
-            userApp.getRoles().add(admin);
-        };
-    }
-
 }
